c6304ce1441187595df9b23433ca63c16d7987fb
Vademado <144839711+Vademado@users.noreply.github.com>
2024-05-16 20:35:13 +0300

    Changing comments hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 int main() {
-    // Declare a variable to store the user's name
+    // Name variable
     std::string name;
 
-    // Prompt the user to input their name
+    // Offer to enter your name
     std::cout << "Input name: ";
 
-    // Read the user's name from the standard input
+    // Reading the name
     std::cin >> name;
 
-    // Print a greeting message with the user's name
+    // Print  message with the user's name
     std::cout << "Hello world from " + name << std::endl;
 
     return 0;
