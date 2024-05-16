587fc3308ad27f40875aaddf25434add5c91c2f0
Vademado <VademadoIU8@yandex.ru>
2024-05-16 19:08:41 +0300

    update hello_world.cpp comments added

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
+#include <iostream>
 
-
-int main()
-{
+int main() {
+    // Declare a variable to store the user's name
     std::string name;
 
+    // Prompt the user to input their name
     std::cout << "Input name: ";
+
+    // Read the user's name from the standard input
     std::cin >> name;
 
+    // Print a greeting message with the user's name
     std::cout << "Hello world from " + name << std::endl;
 
     return 0;
